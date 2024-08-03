de3475097cce903503639f4d13089b293e2ce05d
ariutti <nicola.ariutti@gmail.com>
2024-08-03 13:32:45 +0200

    ficed some typos

--- about/src.php
+++ about/src.php
@@ -3,10 +3,10 @@
 
 
 <?php h("This website");?>
-<p>This website has been created on the code base by [Fabien Sanglard](https://fabiensanglard.net), a really brilliant person who, in his spare time, devotes himself to research mainly in the field of computer science and retro-engineering of the gaming world, but not only.
+<p>This website has been created on the code base by <a href="https://fabiensanglard.net">Fabien Sanglard</a>, a really brilliant person who, in his spare time, devotes himself to research mainly in the field of computer science and retro-engineering of the gaming world, but not only.
 <br/>
 His website and books have been a great source of inspiration for me.<br/>
-You can find a copy of his website code (and also read more about his website techs) [here](https://fabiensanglard.net/ilike/index.html) and [here](https://fabiensanglard.net/html/index.html).
+You can find a copy of his website code (and also read more about his website techs) <a href="https://fabiensanglard.net/ilike/index.html">here</a>, and <a href="https://fabiensanglard.net/html/index.html">here</a>.
 </p>
 <br/>
 <p>Thanks Fabien!</p>
