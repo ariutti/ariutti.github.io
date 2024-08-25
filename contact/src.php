b3ebea9b19dbb0098013f244d804ded5bd79c898
ariutti <nicola.ariutti@gmail.com>
2024-08-25 17:40:30 +0200

    mod contacts

--- contact/src.php
+++ contact/src.php
@@ -5,14 +5,21 @@
   <style type="text/css"> a { text-decoration:none; }</style>
   twitter    : <a href="https://x.com/nicolaariutti">@nicolaariutti</a>
   <br/>
+
   github     : <a href="https://github.com/ariutti/">ariutti</a>
   <br/>
-  freesound  : <a href="https://freesound.org/people/nicola_ariutti/">nicola_ariutti</a>
-  <br/>
-  soundcloud : <a href="https://soundcloud.com/nicolaariutti">nicolaariutti</a>
-  <br/>
-  ccmixter   : <a href="http://ccmixter.org/people/moscardo">moscardo</a>
-  <br/>
-  yt         : <a href="https://www.youtube.com/@NicolaAriutti">@NicolaAriutti</a>
+
+  mail       : <b>my_name</b> dot <b>my_surname</b> at <b>gmail</b> dot <b>com</b>
   <br/>
 </p>
+
+<!--
+freesound  : <a href="https://freesound.org/people/nicola_ariutti/">nicola_ariutti</a>
+<br/>
+soundcloud : <a href="https://soundcloud.com/nicolaariutti">nicolaariutti</a>
+<br/>
+ccmixter   : <a href="http://ccmixter.org/people/moscardo">moscardo</a>
+<br/>
+yt         : <a href="https://www.youtube.com/@NicolaAriutti">@NicolaAriutti</a>
+<br/>
+-->
